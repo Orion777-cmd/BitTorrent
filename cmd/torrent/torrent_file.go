73d7c306af814c1c6953bbd86f3a9b6208a11d32
Orion777-cmd <abiy.biru78@gmail.com>
2024-01-12 21:56:48 +0300

    added bencoding, torrent and tracker functionalites

---
+++ cmd/torrent/torrent_file.go
@@ -0,0 +1,18 @@
+package torrent 
+
+import (
+	"github.com/orion777-cmd/BitTorrent/cmd/bencoding"
+)
+
+type TorrentFile struct {
+	Announce string 
+	InfoHash [20]byte
+	PieceHashes [][20]byte
+	PieceLength int
+	Length int
+	Name int
+}
+
+func (bto bencodeTorrent) toTorrentFile() (TorrentFile, error){
+
+}
